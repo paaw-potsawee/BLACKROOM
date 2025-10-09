cf169577a5cb8302907dd76e0bbed23fd2131b56
Potsawee <potsaweee.p@gmail.com>
2025-10-09 10:26:03 +0700

    improve cli and add insertion functionality in main

--- src/tracking.py
+++ src/tracking.py
@@ -1,6 +1,8 @@
 import psutil
 import os
 from time import time
+from functools import wraps
+from typing import Callable, Optional
 
 
 def process_memory():
@@ -10,17 +12,30 @@ def process_memory():
 
 
 # decorator function allow program to calculate space and time used
-def profile(func):
+def profile(func, *, callback: Optional[Callable] = None, message: Optional[str] = None):
+
+    @wraps(func)
     def wrapper(*args, **kwargs):
+        runtime_msg = kwargs.pop('profile_msg', None)
+        used_msg = runtime_msg or message or func.__name__
 
         mem_before = process_memory()
         start_time = time()
         result = func(*args, **kwargs)
         mem_after = process_memory()
-        print("{}:consumed memory: {:,}".format(
-            func.__name__,
-            mem_before, mem_after, mem_after - mem_before))
-        print(f"{func.__name__}:{(time() - start_time) * 1000} ms")
+        elapsed_ms = (time() - start_time) * 1000
+        mem_diff = mem_after - mem_before
+
+        if callback:
+            try:
+                callback(func.__name, mem_before, mem_after, mem_diff,
+                         elapsed_ms, result, args, kwargs)
+            except:
+                pass
+        else:
+            print(
+                f"{used_msg}: memory {mem_after/1024**2:.2f} MB (diff {mem_diff/1024**2:.2f} MB)")
+            print(f"{used_msg}:{elapsed_ms} ms")
 
         return result
     return wrapper
